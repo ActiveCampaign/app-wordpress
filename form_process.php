cdb50b65a3c3f4eedd7a67ccbaeac1e3201e6d1b
Matt Thommes <matt@activecampaign.com>
2015-04-21 11:10:01 -0500

    Need correct class name for Ajax form submissions.

--- form_process.php
+++ form_process.php
@@ -10,7 +10,7 @@
 	$sync = $_GET["sync"];
 
 	require_once "activecampaign-api-php/ActiveCampaign.class.php";
-	$ac = new ActiveCampaign($api_url, $api_key);
+	$ac = new ActiveCampaignWordPress($api_url, $api_key);
 
 	$form_process = $ac->api("form/process?sync={$sync}");
 
