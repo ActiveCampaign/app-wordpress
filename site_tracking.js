47734b3b3beeabbb05dcdea9b10676cad6207173
ammorrise <amorrise@activecampaign.com>
2019-03-20 13:15:25 -0700

    Update prism to https

--- site_tracking.js
+++ site_tracking.js
@@ -1,5 +1,5 @@
 if (typeof(php_data.ac_settings.site_tracking) != "undefined" && php_data.ac_settings.site_tracking == "1") {
-	(function(e,t,o,n,p,r,i){e.prismGlobalObjectAlias=n;e.pgo=e.pgo||function(){(e.pgo.q=e.pgo.q||[]).push(arguments)};e.pgo.l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"http://prism.app-us1.com/prism.js","pgo");
+	(function(e,t,o,n,p,r,i){e.prismGlobalObjectAlias=n;e.pgo=e.pgo||function(){(e.pgo.q=e.pgo.q||[]).push(arguments)};e.pgo.l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://prism.app-us1.com/prism.js","pgo");
 
 	pgo('setAccount', php_data.ac_settings.tracking_actid);
 	pgo('setTrackByDefault', php_data.ac_settings.site_tracking_default == "1");
